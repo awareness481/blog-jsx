8f0f7ee927ba18167b2e334c2a2261618160842b
Jesse Jafa <jesse.jafa@gmail.com>
2021-10-01 10:58:41 +0300

    Update app.spec.js

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,6 +3,7 @@
 describe("Accessibility", () => {
   it("should log accessibility failures" , () => {
     cy.visit('/');
+    cy.contains('Fail test')
     cy.screenshot();
     cy.injectAxe();
     cy.checkA11y();
