e4a26fe5f366f2383eb7f83eaa98983f97868fda
Jesse Jafa <jesse.jafa@gmail.com>
2022-07-02 08:31:47 +0300

    Update mdxUtils.ts

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-// POSTS_PATH is useful when you want to get the path to a specific file
+// POSTS_PATH is useful when you want to get the path to a specific file 
 export const POSTS_PATH = path.join(process.cwd(), 'posts')
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
